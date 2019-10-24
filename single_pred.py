0c0356803f865d96863596d4a6eff3b383b5f0f4
Hafiz Shehbaz Ali <shehbaz.bsee1395@gmail.com>
2019-10-24 22:36:24 +0400

    Draw rectangle

--- single_pred.py
+++ single_pred.py
@@ -10,7 +10,7 @@ def predict_func(model, inp, name, image_save=False):
     rects = predicted_box(pred_out)
     for rect in rects:
         r_img = draw_rectangle(img, rect)
-
+    print('draw rectangle image')
     show_image(r_img)
     if image_save:
         save_image(r_img, name)
@@ -22,9 +22,9 @@ model.compile(loss=yolo_loss_func, optimizer=optimizer, metrics=['accuracy'])
 # import data
 # X and Y numpy arrays are created using the Prepocess.py file
 X, Y = load_date(data_path=DATA_PATH)
-X_train, X_val, Y_train, Y_val = train_test_split(X, Y, train_size=0.75, shuffle=True)
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, train_size=0.75, shuffle=False)
 X = []
 Y = []
 
-show_image(X_val[0])
-predict_func(model, X_val[0], 'img_0')
+# show_image(X_val[0])
+predict_func(model, X_val[0:1], 'img_0')
